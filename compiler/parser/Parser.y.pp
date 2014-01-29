2ad09c449f5759c80772dcbf0ac6e49e8879dd8e
Facundo Domínguez <facundo.dominguez@tweag.io>
2014-01-29 12:43:03 -0200

    Have the parser understand the static keyword when using the StaticValues extension.

--- compiler/parser/Parser.y.pp
+++ compiler/parser/Parser.y.pp
@@ -250,6 +250,7 @@ incorrect.
  'by'       { L _ ITby }        -- for list transform extension
  'using'    { L _ ITusing }     -- for list transform extension
  'pattern'      { L _ ITpattern } -- for pattern synonyms
+ 'static'       { L _ ITstatic }  -- for static values extension
 
  '{-# INLINE'             { L _ (ITinline_prag _ _) }
  '{-# SPECIALISE'         { L _ ITspec_prag }
@@ -1546,6 +1547,7 @@ hpc_annot :: { Located (FastString,(Int,Int),(Int,Int)) }
 
 fexp    :: { LHsExpr RdrName }
         : fexp aexp                             { LL $ HsApp $1 $2 }
+        | 'static' aexp                         { LL $ HsStatic $2 }
         | aexp                                  { $1 }
 
 aexp    :: { LHsExpr RdrName }
