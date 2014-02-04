3d841874aa7f822918cc13b05aa31b5faed860d1
Facundo Domínguez <facundo.dominguez@tweag.io>
2014-02-04 21:58:22 -0200

    Implement tests of the StaticValues extension.

---
+++ testsuite/tests/deSugar/should_run/DsStaticValues.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE StaticValues #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+
+import GHC.StaticRef
+
+main = putStr $ unlines $ map showGlobalName gNames
+  where
+    globalNameStaticRef (StaticRef gn) = gn
+    showGlobalName (GlobalName pkg _ m n) =
+      unwords $ ("GlobalName" :) $ map show [ pkg, m, n ]
+    gNames =
+      [ globalNameStaticRef $ static g
+      , globalNameStaticRef $ static id
+      , globalNameStaticRef $ static (&&)
+      , globalNameStaticRef $ static method
+      , globalNameStaticRef $ static t_field
+      , globalNameStaticRef $ static (id . show)
+      , globalNameStaticRef $ static (id . (+))
+      ]
+
+g :: Int -> Int
+g = id
+
+class C a where
+  method :: a -> Int
+
+data T a = T { t_field :: a }
