75b8317314afa59f95cc9cd54d2758d7efd67733
Facundo Domínguez <facundo.dominguez@tweag.io>
2014-02-04 21:58:22 -0200

    Implement tests of the StaticValues extension.

---
+++ testsuite/tests/typecheck/should_compile/TcStaticValues02.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE StaticValues #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+
+module StaticValues02 where
+
+import GHC.StaticRef
+
+f2 :: StaticRef (forall a . a -> a)
+f2 = static id
+
+f3 :: StaticRef (forall a . C a => a -> Int)
+f3 = static method
+
+f4 :: StaticRef (forall a . T a -> a)
+f4 = static t_field
+
+g :: Int -> Int
+g = id
+
+class C a where
+  method :: a -> Int
+
+data T a = T { t_field :: a }
