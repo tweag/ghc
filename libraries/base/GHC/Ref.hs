cd6641ee3b842ddd03150ba85810fe3831acf2d4
Facundo Domínguez <facundo.dominguez@tweag.io>
2014-07-21 11:02:15 -0300

    WIP base:GHC.Ref.deRef

--- libraries/base/GHC/Ref.hs
+++ libraries/base/GHC/Ref.hs
@@ -27,10 +27,17 @@
 --
 -----------------------------------------------------------------------------
 
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable       #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE MagicHash                #-}
+{-# LANGUAGE UnboxedTuples            #-}
 module GHC.Ref where
 
-import Data.Typeable (Typeable)
+import Data.Typeable    (Typeable)
+import Encoding         ( zEncodeString )
+import Foreign.C.String ( withCString, CString )
+import GHC.Exts         ( addrToAny# )
+import GHC.Ptr          ( Ptr(..), nullPtr )
 
 -- | A reference to a top-level value of type 'a'.
 data Ref a = Ref { unRef :: GlobalName }
@@ -50,3 +57,44 @@ data Ref a = Ref { unRef :: GlobalName }
 data GlobalName = GlobalName String String String String
   deriving (Show, Typeable)
 
+-- | An unsafe lookup function for symbolic references.
+--
+-- @deRef (r :: Ref a)@ returns @Nothing@ if no associated value is found for
+-- @r@, and @Just v@ if @v@ is the value associated to @r@ and it has the
+-- expected type @a@.
+--
+-- This function is unsafe because if an associated value @v@ of a different
+-- type is found, then the behavior of this function is undefined.
+--
+-- Currently, a value is considered associated to a reference if the module
+-- producing the reference has been dinamically linked to the program which
+-- calls deRef.
+--
+deRef :: Ref a -> IO (Maybe a)
+deRef (Ref (GlobalName pkg _ m n)) = do
+    let mpkg = case pkg of
+                 "main" -> Nothing
+                 _ -> Just pkg
+    loadFunction mpkg m n
+
+-- loadFunction__ taken from
+-- @plugins-1.5.4.0:System.Plugins.Load.loadFunction__@
+loadFunction :: Maybe String
+             -> String
+             -> String
+             -> IO (Maybe a)
+loadFunction mpkg m valsym = do
+    let symbol = prefixUnderscore
+                   ++ maybe "" (\p -> zEncodeString p ++ "_") mpkg
+                   ++ zEncodeString m ++ "_" ++ zEncodeString valsym
+                   ++ "_closure"
+    ptr@(Ptr addr) <- withCString symbol c_lookupSymbol
+    if (ptr == nullPtr)
+    then return Nothing
+    else case addrToAny# addr of
+           (# hval #) -> return ( Just hval )
+  where
+    prefixUnderscore = if elem os ["darwin","mingw32","cygwin"] then "_" else ""
+
+foreign import ccall safe "lookupSymbol"
+   c_lookupSymbol :: CString -> IO (Ptr a)
