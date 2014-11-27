abfdda9d89379655fda0b63c678f11372b365b4a
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-11-27 20:26:11 +0300

    Initial SPT support inside rts

---
+++ includes/rts/SPT.h
@@ -0,0 +1,20 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The GHC Team, 2008-2009
+ *
+ * Haskell Program Coverage
+ *
+ * Do not #include this file directly: #include "Rts.h" instead.
+ *
+ * To understand the structure of the RTS headers, see the wiki:
+ *   http://ghc.haskell.org/trac/ghc/wiki/Commentary/SourceTree/Includes
+ *
+ * -------------------------------------------------------------------------- */
+
+#ifndef RTS_SPT_H
+#define RTS_SPT_H
+
+
+void hs_spt_module (void * entries[]);
+
+#endif /* RTS_HPC_H */
