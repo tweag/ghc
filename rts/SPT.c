c8eea5ef939b6082d6c9ea507e7b0027b57f60f7
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-11-28 19:39:27 +0300

    fixup rts: remove debug output

--- rts/SPT.c
+++ rts/SPT.c
@@ -32,16 +32,11 @@ hs_spt_module(void *spe[])
 
   size_t i;
   for (i=0; spe[i]; i+=2) {
-	printf("save [%" PRIu64 ",%" PRIu64 ",%" PRIu64 "] : %p\n", ((uint64_t*)spe[i])[0], ((uint64_t*)spe[i])[1], ((uint64_t*)spe[i])[2] , spe[i+1]);
     getStablePtr(spe[i+1]);
     insertHashTable(spt, (StgWord)spe[i], spe[i+1]);
   }
 }
 
 StgPtr hs_spt_lookup(char* key) {
-    	uint64_t *spe =(uint64_t*)key;
-	printf("load [%" PRIu64 ",%" PRIu64 ",%" PRIu64 "]", spe[0], spe[1], spe[2]);
-    StgPtr res = lookupHashTable(spt, (StgWord)key);
-    
-	return res;
+    return lookupHashTable(spt, (StgWord)key);
 }
