0e6b612ab08c5522908e7810e32c8a8940e39c60
Facundo Domínguez <facundo.dominguez@tweag.io>
2014-12-21 12:09:27 -0200

    Trac #9878: Use a mutex to protect the spt.

--- rts/StaticPtrTable.c
+++ rts/StaticPtrTable.c
@@ -14,6 +14,10 @@
 
 static HashTable * spt = NULL;
 
+#ifdef THREADED_RTS
+static Mutex spt_lock;
+#endif
+
 /// Hash function for the SPT.
 static int hashFingerprint(HashTable *table, StgWord64 key[2]) {
   // Take half of the key to compute the hash.
@@ -28,21 +32,39 @@ static int compareFingerprint(StgWord64 ptra[2], StgWord64 ptrb[2]) {
 void hs_spt_insert(StgWord64 key[2],void *spe_closure) {
   // hs_spt_insert is called from constructor functions, so
   // the SPT needs to be initialized here.
-  if (spt == NULL)
+  if (spt == NULL) {
     spt = allocHashTable_( (HashFunction *)hashFingerprint
                          , (CompareFunction *)compareFingerprint
                          );
+#ifdef THREADED_RTS
+    initMutex(&spt_lock);
+#endif
+  }
 
   getStablePtr(spe_closure);
+  ACQUIRE_LOCK(&spt_lock);
   insertHashTable(spt, (StgWord)key, spe_closure);
+  RELEASE_LOCK(&spt_lock);
 }
 
 StgPtr hs_spt_lookup(StgWord64 key[2]) {
-  return spt ? lookupHashTable(spt, (StgWord)key) : NULL;
+  if (spt) {
+    ACQUIRE_LOCK(&spt_lock);
+    const StgPtr ret = lookupHashTable(spt, (StgWord)key);
+    RELEASE_LOCK(&spt_lock);
+    return ret;
+  } else
+    return NULL;
 }
 
 int hs_spt_keys(StgPtr keys[], int szKeys) {
-  return spt ? keysHashTable(spt, (StgWord*)keys, szKeys) : 0;
+  if (spt) {
+    ACQUIRE_LOCK(&spt_lock);
+    const int ret = keysHashTable(spt, (StgWord*)keys, szKeys);
+    RELEASE_LOCK(&spt_lock);
+    return ret;
+  } else
+    return 0;
 }
 
 int hs_spt_key_count() {
@@ -53,5 +75,8 @@ void exitStaticPtrTable() {
   if (spt) {
     freeHashTable(spt, NULL);
     spt = NULL;
+#ifdef THREADED_RTS
+    closeMutex(&spt_lock);
+#endif
   }
 }
