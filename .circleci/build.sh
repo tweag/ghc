e8ac8be12612a905f5c85a2d37e5b3dce837dfcc
zimbatm <zimbatm@zimbatm.com>
2017-10-18 13:27:44 +0100

    simplify

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,6 +3,11 @@
 
 set -euo pipefail
 
+fail() {
+  echo "ERROR: $*" >&2
+  exit 1
+}
+
 echo 'BUILD_SPHINX_HTML = NO' > mk/validate.mk
 echo 'BUILD_SPHINX_PDF = NO' >> mk/validate.mk
 
@@ -10,18 +15,29 @@ export THREADS=8
 export SKIP_PERF_TESTS=YES
 export VERBOSE=2
 
-if [[ -n ${TARGET:-} ]]; then
-  if [[ $(uname) != Linux ]]; then
-    fail "uname=$(uname) not supported for cross-compilation"
-  fi
-  if [[ $target != FreeBSD ]]; then
-    fail "TARGET=$TARGET not supported"
-  fi
-  echo 'HADDOCK_DOCS = NO' >> mk/validate.mk
-  echo 'WERROR=' >> mk/validate.mk
-  export PATH=/opt/ghc/bin:$PATH
-  ./boot
-  config_args=--target=x86_64-unknown-freebsd10 ./validate --quiet --build-only
-else
-  ./validate --fast --quiet
-fi
+case "$(uname)" in
+  Linux)
+    if [[ -n ${TARGET:-} ]]; then
+      if [[ $TARGET = FreeBSD ]]; then
+        # cross-compiling to FreeBSD
+        echo 'HADDOCK_DOCS = NO' >> mk/validate.mk
+        echo 'WERROR=' >> mk/validate.mk
+        export PATH=/opt/ghc/bin:$PATH
+        ./boot
+        config_args=--target=x86_64-unknown-freebsd10 ./validate --quiet --build-only
+      else
+        fail "TARGET=$target not supported"
+      fi
+    else
+      ./validate --fast --quiet
+    fi
+    ;;
+  Darwin)
+    if [[ -n ${TARGET:-} ]]; then
+      fail "uname=$(uname) not supported for cross-compilation"
+    fi
+    ./validate --fast --quiet
+    ;;
+  *)
+    fail "uname=$(uname) not supported"
+esac
