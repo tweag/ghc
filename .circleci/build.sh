8a2ffa3af3a23a18746ee908b72c6660e7b3fbb7
zimbatm <zimbatm@zimbatm.com>
2017-10-18 13:15:45 +0100

    stricted target selection

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,23 +3,25 @@
 
 set -euo pipefail
 
-echo 'BUILD_SPHINX_HTML = NO' >> mk/validate.mk
+echo 'BUILD_SPHINX_HTML = NO' > mk/validate.mk
 echo 'BUILD_SPHINX_PDF = NO' >> mk/validate.mk
 
 export THREADS=8
 export SKIP_PERF_TESTS=YES
 export VERBOSE=2
 
-target=${TARGET:-$(uname)}
-case "$target" in
-  FreeBSD)
-    echo 'HADDOCK_DOCS = NO' >> mk/validate.mk
-    echo 'WERROR=' >> mk/validate.mk
-    export PATH=/opt/ghc/bin:$PATH
-    ./boot
-    config_args=--target=x86_64-unknown-freebsd10 ./validate --quiet --build-only
-    ;;
-  *)
-    ./validate --fast --quiet
-    ;;
-esac
+if [[ -n ${TARGET:-} ]]; then
+  if [[ $(uname) != Linux ]]; then
+    fail "uname=$(uname) not supported for cross-compilation"
+  fi
+  if [[ $target != FreeBSD ]]; then
+    fail "TARGET=$TARGET not supported"
+  fi
+  echo 'HADDOCK_DOCS = NO' >> mk/validate.mk
+  echo 'WERROR=' >> mk/validate.mk
+  export PATH=/opt/ghc/bin:$PATH
+  ./boot
+  config_args=--target=x86_64-unknown-freebsd10 ./validate --quiet --build-only
+else
+  ./validate --fast --quiet
+fi
--- .circleci/prepare-system.sh
+++ .circleci/prepare-system.sh
@@ -2,24 +2,39 @@
 # vim: sw=2 et
 set -euo pipefail
 
-target=${TARGET:-$(uname)}
-case "$target" in
+fail() {
+  echo "ERROR: $*" >&2
+  exit 1
+}
+
+case "$(uname)" in
   Linux)
-    apt-get update -qq
-    apt-get install -qy git openssh-client make automake autoconf gcc perl python3 texinfo
-    ;;
-  FreeBSD)
-    # yes, it's cross-compiling
-    add-apt-repository ppa:hvr/ghc
-    apt-get update -qq
-    apt-get install -qy ghc-8.0.2 alex happy ncurses-dev
+    if [[ -n ${TARGET:-} ]]; then
+      if [[ $TARGET = FreeBSD ]]; then
+        # cross-compiling to FreeBSD
+        add-apt-repository -y ppa:hvr/ghc
+        apt-get update -qq
+        apt-get install -qy ghc-8.0.2 alex happy ncurses-dev
+      else
+        fail "TARGET=$target not supported"
+      fi
+    else
+      # assuming Ubuntu
+      apt-get update -qq
+      apt-get install -qy git openssh-client make automake autoconf gcc perl python3 texinfo
+    fi
     ;;
   Darwin)
-    brew install ghc cabal-install python3
+    if [[ -n ${TARGET:-} ]]; then
+      fail "uname=$(uname) not supported for cross-compilation"
+    fi
+    brew install ghc cabal-install python3 ncurses
     cabal update
     cabal install alex happy haddock
+    # put them on the $PATH
     ln -s $HOME/.cabal/bin/alex /usr/local/bin/alex
     ln -s $HOME/.cabal/bin/happy /usr/local/bin/happy
     ;;
+  *)
+    fail "uname=$(uname) not supported"
 esac
-
