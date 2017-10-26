27d2208bb1dbe4830ad5ffca2800d470f1f275b5
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2017-10-26 16:39:21 +0100

    .

--- .circleci/prepare-system.sh
+++ .circleci/prepare-system.sh
@@ -17,7 +17,8 @@ case "$(uname)" in
         apt-get install -qy ghc-8.0.2 cabal-install  alex happy ncurses-dev git openssh-client make automake autoconf gcc perl python3 texinfo xz-utils
         cabal update
         cabal install --reinstall hscolour
-        echo $PATH
+        echo $PATH $HOME
+        find $HOME/.cabal
         ln -s $HOME/.cabal/bin/hscolour /usr/local/bin/hscolour
         which hscolour
       else
