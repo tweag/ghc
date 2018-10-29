0eb7ff4d78ef8359a4d118add2e30c105207face
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2018-10-29 17:11:38 +0100

    Levity polymorphic data type have no wrappers
    Because such wrappers would usually violate the levity-polymorphism
    restriction (_i.e._ no variable have levity-polymorphic types).
    
    We could probably make the test more precise (only targeting data
    constructors which actually have levity polymorphic fields), but this
    should be fine for now.

--- compiler/basicTypes/DataCon.hs
+++ compiler/basicTypes/DataCon.hs
@@ -916,12 +916,10 @@ mkDataCon name declared_infix prom_info
     rep_arg_tys = dataConRepArgTys con
 
     rep_ty =
-      -- TODO: arnaud: if we stay on the mode that all data constructors have
-      -- wrappers, then we need to remove this test.
       case rep of
         -- If the DataCon has no wrapper, then the worker's type *is* the
         -- user-facing type, so we can simply use dataConUserType
---        NoDataConRep -> dataConUserType con
+        NoDataConRep -> dataConUserType con
         -- If the DataCon has a wrapper, then the worker's type is never seen
         -- by the user. The visibilities we pick do not matter here.
         _ -> mkInvForAllTys univ_tvs $ mkInvForAllTys ex_tvs $
@@ -1090,7 +1088,7 @@ dataConWrapId_maybe dc = case dcRep dc of
 -- the worker (see 'dataConWorkId')
 dataConWrapId :: DataCon -> Id
 dataConWrapId dc = case dcRep dc of
-                     NoDataConRep-> panic "dataConWrapId"
+                     NoDataConRep-> dcWorkId dc    -- worker=wrapper
                      DCR { dcr_wrap_id = wrap_id } -> wrap_id
 
 -- | Find all the 'Id's implicitly brought into scope by the data constructor. Currently,
