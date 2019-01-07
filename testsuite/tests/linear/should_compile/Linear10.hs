59837d8cc163819d47902e68e79dece5f8a9401d
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2019-01-07 16:51:17 +0100

    Clean up unexpected passes
    I've checked the history to make sure the tests make sense:
    
    - The code in DollarTest for 'foo' was not used; it now
      correctly doesn't compile, as $ doesn't support polymorphism
    - OldList was causing failures: https://github.com/tweag/ghc/issues/97
    - CSETest was causing core lint error: 8a968b561e74a1
    - Op was passing; it was not detected as 'unexpected pass'
      because it contained a tab character that triggered a warning
    - Linear10 is passing after removing an incorrect newtype GADT;
      we are testing for it in LinearGADTNewtype.
    
    This leaves Linear4 and Linear14 broken; I've created #298 to track
    this.

--- testsuite/tests/linear/should_compile/Linear10.hs
+++ testsuite/tests/linear/should_compile/Linear10.hs
@@ -7,8 +7,3 @@ data Unrestricted a where Unrestricted :: a -> Unrestricted a
 
 unrestrictedDup :: Unrestricted a ⊸ (a, a)
 unrestrictedDup (Unrestricted a) = (a,a)
-
-newtype Unrestricted2 a where Unrestricted2 :: a -> Unrestricted2 a
-
-unrestricted2Dup :: Unrestricted2 a ⊸ (a,a)
-unrestricted2Dup (Unrestricted2 a) = (a,a)
