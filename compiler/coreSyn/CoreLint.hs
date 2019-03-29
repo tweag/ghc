tweag/ghc

commit 889b2fd9a17b03d8b48c6cae88490a3a38a90c04

Move Zero multiplicity to a separate type

compiler/coreSyn/CoreLint.hs
@@ -951,9 +951,10 @@ checkJoinOcc var n_args
 checkLinearity :: UsageEnv -> Var -> LintM UsageEnv
 checkLinearity body_ue lam_var =
   case varMultMaybe lam_var of
-    Just (Regular mult) -> do
-      ensureEqMults (lookupUE body_ue lam_var) mult (err_msg mult)
-      return $ deleteUE body_ue lam_var
+    Just (Regular mult) -> do case lhs of
+                                 MUsage m -> ensureEqMults m mult (err_msg mult)
+                                 Zero -> addErrL (err_msg mult)
+                              return $ deleteUE body_ue lam_var
     Just Alias -> return body_ue -- aliases do not generate multiplicity constraints
     Nothing    -> return body_ue -- A type variable
   where
@@ -1089,13 +1090,15 @@ lintAltBinders rhs_ue scrut scrut_ty con_ty ((var_w, bndr):bndrs)
        ; lintAltBinders rhs_ue' scrut scrut_ty con_ty' bndrs }
 
 -- | Implements the case rules for linearity
-checkCaseLinearity :: UsageEnv -> Var ->  Mult -> Var -> LintM UsageEnv
+checkCaseLinearity :: UsageEnv -> Var -> Mult -> Var -> LintM UsageEnv
 checkCaseLinearity ue scrut var_w bndr = do
-  ensureEqMults lhs rhs err_msg
+  case lhs of
+    MUsage mult -> ensureEqMults mult rhs err_msg
+    Zero -> addErrL err_msg
   lintLinearBinder (ppr bndr) (scrut_w `MultMul` var_w) (varWeight bndr)
   return $ deleteUE ue bndr
   where
-    lhs = bndr_usage `MultAdd` (scrut_usage `MultMul` var_w)
+    lhs = bndr_usage `addUsage` (scrut_usage `multUsage` (MUsage var_w))
     rhs = scrut_w `MultMul` var_w
     err_msg  = (text "Linearity failure in variable:" <+> ppr bndr
                 $$ ppr lhs <+> text "⊈" <+> ppr rhs

compiler/basicTypes/Multiplicity.hs
@@ -7,7 +7,6 @@
 -- arrow (in the sense of linear types).
 module Multiplicity
   ( GMult
-  , pattern Zero
   , pattern One
   , pattern Omega
   , pattern MultAdd
@@ -40,8 +39,7 @@ import Outputable
 --
 
 data GMult a
-  = Zero_
-  | One_
+  = One_
   | Omega_
   | MultAdd_ (GMult a) (GMult a)
   | MultMul_ (GMult a) (GMult a)
@@ -60,16 +58,13 @@ class Multable a where
   -- that @m@ is a subtree of @toMult . fromMult m@.
   toMult :: a -> GMult a
 
--- Note that pattern synonyms for One, Omega, and Zero are not necessary: we could just
+-- Note that pattern synonyms for One and Omega are not necessary: we could just
 -- export them as constructors. They are defined as pattern synonym for
 -- symmetry. Following the principle of least surprise.
 
 -- We may enforce more invariants in the type of GMult. For instance, we can
 -- enforce that it is in the form of a sum of products, and even that the
 -- sumands and factors are ordered somehow, to have more equalities.
-
-pattern Zero :: GMult a
-pattern Zero = Zero_
 
 pattern One :: GMult a
 pattern One = One_
@@ -79,8 +74,6 @@ pattern Omega = Omega_
 
 pattern MultMul :: GMult a -> GMult a -> GMult a
 pattern MultMul p q <- MultMul_ p q where
-  Zero `MultMul` _ = Zero
-  _ `MultMul` Zero = Zero
   One `MultMul` p = p
   p `MultMul` One = p
   Omega `MultMul` _ = Omega
@@ -89,8 +82,6 @@ pattern MultMul p q <- MultMul_ p q where
 
 pattern MultAdd :: GMult a -> GMult a -> GMult a
 pattern MultAdd p q <- MultAdd_ p q where
-  Zero `MultAdd` p = p
-  p `MultAdd` Zero = p
   One `MultAdd` One = Omega
   Omega `MultAdd` _ = Omega
   _ `MultAdd` Omega = Omega
@@ -100,7 +91,7 @@ pattern MultThing :: Multable a => a -> GMult a
 pattern MultThing a <- MultThing_ a where
   MultThing a = toMult a
 
-{-# COMPLETE Zero, One, Omega, MultMul, MultAdd, MultThing #-}
+{-# COMPLETE One, Omega, MultMul, MultAdd, MultThing #-}
 
 -- | Used to defined 'Multable' instances. Requires that the argument cannot be
 -- reified any further. There is probably no good reason to use it outside of a
@@ -109,7 +100,6 @@ unsafeMultThing :: a -> GMult a
 unsafeMultThing = MultThing_
 
 instance (Outputable a, Multable a) => Outputable (GMult a) where
-  ppr Zero = text "0"
   ppr One = text "1"
   ppr Omega = text "Omega"
   ppr (MultAdd m1 m2) = parens (ppr m1 <+> text "+" <+> ppr m2)
@@ -119,7 +109,6 @@ instance (Outputable a, Multable a) => Outputable (GMult a) where
 -- | @sup w1 w2@ returns the smallest multiplicity larger than or equal to both @w1@
 -- and @w2@.
 sup :: GMult a -> GMult a -> GMult a
-sup Zero  Zero  = Zero
 sup One   One   = One
 sup Omega Omega = Omega
 sup _     _     = Omega
@@ -173,26 +162,17 @@ instance Outputable IsSubmult where
 
 -- | @submult w1 w2@ check whether a value of multiplicity @w1@ is allowed where a
 -- value of multiplicity @w2@ is expected. This is a partial order.
+
 submult :: GMult t -> GMult t -> IsSubmult
 submult _     Omega = Submult
-submult Zero  Zero  = Submult
-submult _     Zero  = NotSubmult
 submult Omega One   = NotSubmult
-submult Zero  One   = NotSubmult
--- It is no mistake: 'Zero' is not a submult of 'One': a value which must be
--- used zero times cannot be used one time.
--- Zero = {0}
--- One  = {1}
--- Omega = {0...}
 submult One   One   = Submult
 -- The 1 <= p rule
 submult One   _     = Submult
 --    submult (MultThing t) (MultThing t') = Unknown
 submult _     _     = Unknown
 
-
 traverseMult :: (Multable t, Multable u, Applicative f) => (t -> f u) -> GMult t -> f (GMult u)
-traverseMult _ Zero = pure Zero
 traverseMult _ One = pure One
 traverseMult _ Omega = pure Omega
 traverseMult f (MultThing t) = MultThing <$> f t
@@ -201,8 +181,7 @@ traverseMult f (MultMul x y) = MultMul <$> traverseMult f x <*> traverseMult f y
 
 multThingList :: Multable t => (t -> a) -> GMult t -> [a]
 multThingList f = go []
-  where go acc Zero = acc
-        go acc One = acc
+  where go acc One = acc
         go acc Omega = acc
         go acc (MultThing t) = f t : acc
         go acc (MultAdd x y) = go (go acc y) x
@@ -210,7 +189,6 @@ multThingList f = go []
 
 -- Not a Functor, since MultThing calls 'fromMult'.
 mapMult :: (Multable t, Multable u) => (t -> u) -> GMult t -> GMult u
-mapMult _ Zero = Zero
 mapMult _ One = One
 mapMult _ Omega = Omega
 mapMult f (MultThing t) = MultThing (f t)

compiler/coreSyn/CoreMap.hs
@@ -536,7 +536,6 @@ instance Eq (DeBruijn Type) where
         _ -> False
 
 instance (Multable a, Eq (DeBruijn a)) => Eq (DeBruijn (GMult a)) where
-  (D _ Zero) == (D _ Zero) = True
   (D _ One) == (D _ One) = True
   (D _ Omega) == (D _ Omega) = True
   (D env (MultAdd p q)) == (D env' (MultAdd p' q')) = (D env p) == (D env' p') && (D env q) == (D env' q')

compiler/typecheck/TcEnv.hs
@@ -609,7 +609,7 @@ tc_extend_local_env top_lvl extra_env thing_inside
 
     check_binder :: Mult -> Name -> UsageEnv -> TcM UsageEnv
     check_binder w x uenv = do
-      let actual_w = lookupUE uenv x
+      let actual_w = case lookupUE uenv x of MUsage u -> u; Zero -> Omega
       traceTc "check_binder" (ppr w $$ ppr actual_w)
       case submult actual_w w of
         Submult -> return ()

compiler/typecheck/TcMType.hs
@@ -1179,7 +1179,6 @@ collect_cand_qtvs :: Bool   -- True <=> consider every fv in Type to be dependen
 collect_cand_qtvs is_dep bound dvs ty
   = go dvs ty
   where
-    go_mult dv Zero  = return dv
     go_mult dv One   = return dv
     go_mult dv Omega = return dv
     go_mult dv (MultAdd x y) = foldlM go_mult dv [x, y]

compiler/typecheck/TcPat.hs
@@ -370,7 +370,7 @@ tc_pat _ (WildPat _) pat_ty thing_inside
         ; return (WildPat pat_ty, res) }
     where
       checkLinearity =
-        tcSubMult Zero (scaledMult pat_ty)
+        tcSubMult Omega (scaledMult pat_ty)
 
 
 tc_pat penv (AsPat x (dL->L nm_loc name) pat) pat_ty thing_inside

compiler/typecheck/TcUnify.hs
@@ -813,12 +813,10 @@ tc_sub_type_ds eq_orig inst_orig ctxt ty_actual ty_expected
 
 tcEqMult :: CtOrigin -> CtOrigin -> UserTypeCtxt -> Mult -> Mult -> TcM ()
 tcEqMult eq_orig inst_orig ctxt w_actual w_expected = do
-  { let w_actual' = case w_actual of
-                       Zero -> Omega  -- Approximate Zero by Omega (see test LinearPolyType)
-                       _ -> w_actual
+  {
   -- Note that here we do not call to `submult`, so we check
   -- for strict equality.
-  ; _wrap <- tc_sub_type_ds eq_orig inst_orig ctxt (fromMult w_actual') (fromMult w_expected)
+  ; _wrap <- tc_sub_type_ds eq_orig inst_orig ctxt (fromMult w_actual) (fromMult w_expected)
   -- I don't know why, but `_wrap` need not be an identity wrapper. At any rate,
   -- the wrapper isn't significant for multiplicities, so it is safe to drop
   -- it. But maybe there is a better way to implement this function.

compiler/types/FamInstEnv.hs
@@ -1557,7 +1557,6 @@ coreFlattenTy = go
     go env (CoercionTy co) = let (env', co') = coreFlattenCo env co in
                              (env', CoercionTy co')
 
-    go_mult env Zero = (env, Zero)
     go_mult env One = (env, One)
     go_mult env Omega = (env, Omega)
     go_mult env (MultThing t) = let (env', t') = go env t

commit bb28143a524202022719700955bf8a3ff74b6108

Fix mistake in lint
Introduced in 889b2fd9a17, I was probably thinking that ensureEqMults
is checking for equality rather than submultiplicity.

compiler/coreSyn/CoreLint.hs
@@ -1091,13 +1091,14 @@ lintAltBinders rhs_ue scrut scrut_ty con_ty ((var_w, bndr):bndrs)
 -- | Implements the case rules for linearity
 checkCaseLinearity :: UsageEnv -> Var -> Mult -> Var -> LintM UsageEnv
 checkCaseLinearity ue scrut var_w bndr = do
-  case lhs of
-    MUsage mult -> ensureEqMults mult rhs err_msg
-    Zero -> addErrL err_msg
+  ensureEqMults lhs' rhs err_msg
   lintLinearBinder (ppr bndr) (scrut_w `mkMultMul` var_w) (varWeight bndr)
   return $ deleteUE ue bndr
   where
     lhs = bndr_usage `addUsage` (scrut_usage `multUsage` (MUsage var_w))
+    lhs' = case lhs of
+             MUsage mult -> mult
+             Zero -> Omega
     rhs = scrut_w `mkMultMul` var_w
     err_msg  = (text "Linearity failure in variable:" <+> ppr bndr
                 $$ ppr lhs <+> text "⊈" <+> ppr rhs