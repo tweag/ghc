4878e7dece45f8beaed34dadfe241322f38c165d
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2019-04-17 19:36:19 +0200

    Sync lint with tcSubMult

--- compiler/typecheck/TcUnify.hs
+++ compiler/typecheck/TcUnify.hs
@@ -823,6 +823,8 @@ tcEqMult eq_orig inst_orig ctxt w_actual w_expected = do
 
 -- As an approximation to checking w1 * w2 <= w we check that w1 <= w and
 -- w2 <= w. As together they imply that w1 * w2 <= w.
+--
+-- Keep in sync with ensureSubMult in Lint.
 tcSubMult :: CtOrigin -> Mult -> Mult -> TcM ()
 tcSubMult o (MultMul m1 m2) w = tcSubMult o m1 w >> tcSubMult o m2 w
 tcSubMult o a_w c_w = tcEqMult o o TypeAppCtxt a_w c_w
