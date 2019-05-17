23f0cab09b3392ef20ce1549871efe67344d4ba8
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2019-05-17 20:41:03 +0200

    Hack conLikeResTy

--- compiler/basicTypes/ConLike.hs
+++ compiler/basicTypes/ConLike.hs
@@ -37,7 +37,7 @@ import Name
 import BasicTypes
 import TyCoRep (Type, ThetaType)
 import Var
-import Type (mkTyConApp)
+import Type (mkTyConApp, getRuntimeRep)
 
 import qualified Data.Data as Data
 
@@ -144,8 +144,9 @@ conLikeImplBangs (PatSynCon pat_syn)    =
     replicate (patSynArity pat_syn) HsLazy
 
 -- | Returns the type of the whole pattern
-conLikeResTy :: ConLike -> [Type] -> Type
-conLikeResTy (RealDataCon con) tys = mkTyConApp (dataConTyCon con) tys
+conLikeResTy :: HasCallStack => ConLike -> [Type] -> Type
+conLikeResTy (RealDataCon con) tys | isUnboxedTupleCon con || isUnboxedSumCon con, length tys /= length (dataConUnivTyVars con) = (mkTyConApp (dataConTyCon con) (map getRuntimeRep tys ++ tys))
+                                   | otherwise = mkTyConApp (dataConTyCon con) tys
 conLikeResTy (PatSynCon ps)    tys = patSynInstResTy ps tys
 
 -- | The \"full signature\" of the 'ConLike' returns, in order:
