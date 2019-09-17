0f24c904dc7fd745ce7dc29827a91ab1c0231386
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2019-09-17 15:29:57 +0200

    Revert "Allow FUN in GADT declarations"
    This reverts commit d8f720e1f572b6734b7270061e2cc379f89e4f6e
    (issue #214).
    
    As discussed on #216, we don't want infix FUN in data decls:
    
    > data T m a where MkT :: FUN m a (T m a)

--- compiler/hsSyn/HsTypes.hs
+++ compiler/hsSyn/HsTypes.hs
@@ -91,7 +91,6 @@ import RdrName ( RdrName )
 import DataCon( HsSrcBang(..), HsImplBang(..),
                 SrcStrictness(..), SrcUnpackedness(..) )
 import TysWiredIn( unrestrictedFunTyConName, omegaDataConName, oneDataConName )
-import TysPrim( funTyConName )
 import Type
 import HsDoc
 import BasicTypes
@@ -1209,10 +1208,6 @@ splitHsFunType orig_ty@(L _ (HsAppTy _ t1 t2))
                                  , [t1,t2] <- tys
                                  , (args, res) <- splitHsFunType t2
                                  = (hsUnrestricted t1:args, res)
-    go (L _ (HsTyVar _ _ (L _ fn))) tys | fn == funTyConName
-                                 , [tm,t1,t2] <- tys
-                                 , (args, res) <- splitHsFunType t2
-                                 = (HsScaled (HsExplicitMult tm) t1:args, res)
     go (L _ (HsAppTy _ t1 t2)) tys = go t1 (t2:tys)
     go (L _ (HsParTy _ ty))    tys = go ty tys
     go _                       _   = ([], orig_ty)  -- Failure to match
--- testsuite/tests/gadt/T9096a.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE GADTs, DataKinds #-}
-
-module T9096a where
-
-import GHC.Types
-import GHC.Prim
-
-data Foo a where
-  MkFoo :: FUN Omega a (Foo a)
