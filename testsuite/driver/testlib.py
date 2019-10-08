2807fa97be5e69ae42b03ae65858c3558137f24e
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2019-10-08 19:42:39 +0200

    Remove vta_broken

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -102,8 +102,6 @@ def normal( name, opts ):
 def skip( name, opts ):
     opts.skip = True
 
-vta_broken = normal  # TODO
-
 def expect_fail( name, opts ):
     # The compiler, testdriver, OS or platform is missing a certain
     # feature, and we don't plan to or can't fix it now or in the
