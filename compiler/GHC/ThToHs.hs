8434ead488d85dd64e5915154876adc6e590fd34
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-05-28 21:03:22 +0000

    Implement TH support for QualifiedDo.

--- compiler/GHC/ThToHs.hs
+++ compiler/GHC/ThToHs.hs
@@ -934,8 +934,8 @@ cvtl e = wrapL (cvt e)
                             ; th_origin <- getOrigin
                             ; return $ HsCase noExtField e'
                                                  (mkMatchGroup th_origin ms') }
-    cvt (DoE ss)       = cvtHsDo (DoExpr Nothing) ss
-    cvt (MDoE ss)      = cvtHsDo (MDoExpr Nothing) ss
+    cvt (DoE m ss)     = cvtHsDo (DoExpr (mk_mod <$> m)) ss
+    cvt (MDoE m ss)    = cvtHsDo (MDoExpr (mk_mod <$> m)) ss
     cvt (CompE ss)     = cvtHsDo ListComp ss
     cvt (ArithSeqE dd) = do { dd' <- cvtDD dd
                             ; return $ ArithSeq noExtField Nothing dd' }
--- testsuite/tests/th/TH_invalid_add_top_decl.hs
+++ testsuite/tests/th/TH_invalid_add_top_decl.hs
@@ -3,6 +3,6 @@ import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
 $(do
-  invalidDecl <- valD (varP (mkName "emptyDo")) (normalB (doE [])) []
+  invalidDecl <- valD (varP (mkName "emptyDo")) (normalB (doE Nothing [])) []
   addTopDecls [invalidDecl]
   return [])
