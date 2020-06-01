9fd988c7e38d0bb61890237b7ffce12259d864a5
Richard Eisenberg <rae@richarde.dev>
2020-06-01 14:41:47 +0000

    Use nominal role for mult consistently

--- compiler/GHC/Tc/Solver/Flatten.hs
+++ compiler/GHC/Tc/Solver/Flatten.hs
@@ -1163,7 +1163,7 @@ flatten_one (TyConApp tc tys)
 flatten_one ty@(FunTy _ mult ty1 ty2)
   = do { (xi1,co1) <- flatten_one ty1
        ; (xi2,co2) <- flatten_one ty2
-       ; (xi3,co3) <- flatten_one mult
+       ; (xi3,co3) <- setEqRel NomEq $ flatten_one mult
        ; role <- getRole
        ; return (ty { ft_mult = xi3, ft_arg = xi1, ft_res = xi2 }
                 , mkFunCo role co3 co1 co2) }
